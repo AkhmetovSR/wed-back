d8762f759dd5dd62d43cf5d912e77652e5def988
ahmetov <ahmetov26@gmail.com>
2025-02-23 23:37:10 +0500

    home 21 02 2025

--- models/newlywed.js
+++ models/newlywed.js
@@ -2,7 +2,7 @@ const db = require("../config/db")
 const e = require("express");
 class Newlywed {
     static async getData(request) {
-        const sql = "SELECT * FROM wed.newlywed;"
+        const sql = "SELECT * FROM wed.newlywed left join wed.template on newlywed.templateId = template.id;"
         return new Promise((resolve) => {
             db.query(sql, (err, result) => {
                 if (!err) {
--- routes/router.js
+++ routes/router.js
@@ -5,16 +5,12 @@ const cors = require("cors");
 const PORT = 89;
 const app = express();                                            //Express - веб фреймворк для node.js
 app.listen(PORT)
-app.use(cors({cors, optionsSuccessStatus: 200}))                            // Убрать данную строку из Deploy
-// app.use(cors({origin: 'https://lbrty.space', optionsSuccessStatus: 200}))   // Включить данную строку в Deploy
-// app.get("/test", userController.test);
-// app.post("/api/getUser", userController.getUser);
-// app.post("/api/addNewUserAndStartMining", userController.addUser);
-// app.post("/api/claimProfit", userController.claimProfit);
-// app.post("/api/getEarns", userController.getEarns);
-// app.post("/api/EarnComplete", userController.EarnComplete);
+app.use(cors({
+    origin: '*', // Разрешить запросы с любого источника (для тестирования)
+    methods: 'GET,POST', // Разрешенные HTTP-методы
+    optionsSuccessStatus: 200 // Для старых браузеров
+}));                           // Убрать данную строку из Deploy
 
-app.get("/api/dataWed", newlywedController.getAllData);
-// app.get("/api/getAllCompany", companyController.getAllCompany);
+app.get("/api/inf", newlywedController.getAllData);
 
 module.exports = router;
